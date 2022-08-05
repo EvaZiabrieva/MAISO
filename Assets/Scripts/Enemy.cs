a057602cfab13d805691c7b089d3b886139f9223
Eva Ziabrieva <evazyabrevaaa@gmail.com>
2022-08-05 16:46:07 +0300

    add enemy movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float health = 100f;
+    public Transform player;
+    public float moveSpeed = 10f;
+    private Rigidbody2D rb;
+
     public void TakeDamage(float damage)
     {
         health -= damage;
@@ -18,5 +22,17 @@ public class Enemy : MonoBehaviour
     private void Die()
     {
         Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 direction = player.position - transform.position;
+        direction.Normalize();
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
     }
 }
