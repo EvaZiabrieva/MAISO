af3168fdbcc2bfadf92bac733c3727f7ed50c2d4
Eva Ziabrieva <evazyabrevaaa@gmail.com>
2022-08-12 14:23:30 +0300

    add enemy damage

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
 
 public class Enemy : MonoBehaviour
 {
     public float health = 100f;
     public Transform player;
     public float moveSpeed = 10f;
+    public float damage = 5f;
+    public float timeToDamage = 5f;
+
     private Rigidbody2D rb;
+    private float timer = 0f;
 
     public void TakeDamage(float damage)
     {
@@ -31,8 +36,23 @@ public class Enemy : MonoBehaviour
 
     private void Update()
     {
-        Vector2 direction = player.position - transform.position;
-        direction.Normalize();
-        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+        if (player != null)
+        {
+            Vector2 direction = player.position - transform.position;
+            direction.Normalize();
+            rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+            timer += Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D hitInfo)
+    {
+        Player player = hitInfo.GetComponent<Player>();
+
+        if (player != null && timer >= timeToDamage)
+        {
+            player.TakeDamage(damage);
+            timer = 0f;
+        }
     }
 }
